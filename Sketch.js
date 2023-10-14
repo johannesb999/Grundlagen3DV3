95192b40688570210b4dddb30446db582287b7bf
Johannes Biess <128609732+johannesb999@users.noreply.github.com>
2023-10-15 00:05:06 +0200

    Update Sketch.js

--- Sketch.js
+++ Sketch.js
@@ -1,3 +1,11 @@
+let crimeRateData;
+let giniIndexData;
+
+function preload() {
+  crimeRateData = loadTable("path/to/crimerate.csv", "csv", "header");
+  giniIndexData = loadTable("path/to/giniindex.csv", "csv", "header");
+}
+
 function setup() {
   createCanvas(400, 400);
 }
@@ -5,3 +13,38 @@ function draw() {
   background(220);
   ellipse(mouseX, mouseY, 50, 50);
 }
+
+class Countrys {
+  constructor() {
+    this.crimeRates = [];
+    this.giniIndexes = [];
+  }
+
+  getDataByYear(year, dataType) {
+    const dataForYear = [];
+    const data = dataType === "crime" ? this.crimeRates : this.giniIndexes;
+
+    data.forEach((row) => {
+      if (parseInt(row["Year"]) === year) {
+        dataForYear.push(row);
+      }
+    });
+
+    return dataForYear;
+  }
+
+  // ... (more methods can be added)
+}
+
+// Usage example
+const countrys = new Countrys();
+Promise.all([
+  countrys.loadCrimeRates("path/to/crimerate.csv"),
+  countrys.loadGiniIndexes("path/to/giniindex.csv"),
+]).then(() => {
+  const crimeDataForYear = countrys.getDataByYear(2000, "crime");
+  const giniDataForYear = countrys.getDataByYear(2000, "gini");
+
+  console.log(`Crime data for the year 2000:`, crimeDataForYear);
+  console.log(`Gini index data for the year 2000:`, giniDataForYear);
+});
