f3418215433d585b00a8f6b6e69599897181f2e7
Johannes Biess <128609732+johannesb999@users.noreply.github.com>
2023-10-15 04:37:36 +0200

    update

--- Sketch.js
+++ Sketch.js
@@ -9,6 +9,7 @@ let tooltip;
 let tooltipText;
 let yearSlider;
 let yearDisplay;
+let showLabels = false;
 
 function preload() {
   crimeRateData = loadTable("data/crimerate.csv", "csv", "header");
@@ -16,8 +17,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1500, 773);
-  //slider
+  createCanvas(windowWidth, windowHeight); //slider
   yearSlider = select("#yearSlider");
   yearDisplay = select("#yearDisplay");
   yearSlider.input(updateYearData); // Funktion, die bei Änderungen am Slider aufgerufen wird
@@ -39,14 +39,95 @@ function setup() {
   tooltip = document.getElementById("tooltip");
   tooltipText = document.getElementById("tooltipText");
 }
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggleButton");
+  const contentToToggle = document.getElementById("contentToToggle");
+
+  toggleButton.addEventListener("click", function () {
+    if (contentToToggle.style.display === "none") {
+      contentToToggle.style.display = "block";
+      showLabels = true; // Beschriftung anzeigen
+    } else {
+      contentToToggle.style.display = "none";
+      showLabels = false; // Beschriftung ausblenden
+    }
+  });
+});
 
 function updateYearData() {
-  let currentYear = parseInt(yearSlider.value()); // Aktuellen Wert des Sliders holen
-  yearDisplay.html(currentYear); // Aktuellen Wert im Display anzeigen
+  let currentYear = parseInt(yearSlider.value());
+  yearDisplay.html(currentYear);
 
-  // Daten für das ausgewählte Jahr holen
   giniData = countrys.getDataByYear(currentYear, "gini");
   crimeData = countrys.getDataByYear(currentYear, "crime");
+
+  // debugging
+  if (giniData.length !== crimeData.length) {
+    console.warn(
+      `Datenlängen stimmen für das Jahr ${currentYear} nicht überein: Gini(${giniData.length}) und Kriminalität(${crimeData.length})`
+    );
+
+    // Speichert die Ländercodes aus beiden Datensätzen
+    const giniCountries = giniData.map((entry) =>
+      entry.getString("Country Code")
+    );
+    const crimeCountries = crimeData.map((entry) =>
+      entry.getString("Country Code")
+    );
+
+    // Findet die fehlenden oder zusätzlichen Einträge
+    const missingInGini = crimeCountries.filter(
+      (code) => !giniCountries.includes(code)
+    );
+    const missingInCrime = giniCountries.filter(
+      (code) => !crimeCountries.includes(code)
+    );
+
+    console.log(
+      `Fehlend in Gini-Daten für das Jahr ${currentYear}:`,
+      missingInGini
+    );
+    console.log(
+      `Fehlend in Kriminalitätsdaten für das Jahr ${currentYear}:`,
+      missingInCrime
+    );
+
+    // Neue Debugging-Code hier einfügen
+    giniData.forEach((giniEntry, index) => {
+      const giniCountry = giniEntry.getString("Country Code");
+      const giniValue = giniEntry.getNum("GINI Index");
+
+      const crimeEntry = crimeData[index];
+      const crimeCountry = crimeEntry
+        ? crimeEntry.getString("Country Code")
+        : null;
+      const crimeValue = crimeEntry ? crimeEntry.getNum("Homiciderate") : null;
+
+      if (giniCountry !== crimeCountry) {
+        console.error(
+          `Mismatch at index ${index}: GINI country is ${giniCountry}, Crime country is ${crimeCountry}`
+        );
+      }
+
+      if (isNaN(giniValue)) {
+        console.warn(
+          `Invalid GINI value at index ${index} for country ${giniCountry}`
+        );
+      }
+
+      if (isNaN(crimeValue)) {
+        console.warn(
+          `Invalid Crime value at index ${index} for country ${crimeCountry}`
+        );
+      }
+    });
+    // Ende des neuen Debugging-Codes
+  }
+  // debugging ende
 }
 
 function draw() {
@@ -56,6 +137,21 @@ function draw() {
   stroke(255);
   line(300, 100, 300, 650); // Gini-Säule
   line(1200, 100, 1200, 650); // Crime-Säule
+  if (showLabels) {
+    // Beschriftungen für die Gini-Säule hinzufügen
+    fill(255); // Textfarbe
+    textSize(12); // Textgröße
+    for (let i = 0; i <= 100; i += 10) {
+      let y = map(i, 0, 100, 650, 100);
+      text(i, 280, y);
+    }
+
+    // Beschriftungen für die Crime-Säule hinzufügen
+    for (let i = 0; i <= 100; i += 10) {
+      let y = map(i, 0, 100, 650, 100);
+      text(i, 1220, y);
+    }
+  }
 
   //   Linie zwischen den Punkten
   for (let i = 0; i < giniData.length; i++) {
@@ -103,13 +199,21 @@ function mouseMoved() {
 
       if (distance < 5) {
         // Wenn der Abstand klein genug ist, zeigen Sie den Tooltip an
-        tooltipText.innerHTML = `Country: ${giniData[i].getString(
-          "Entity"
-        )} <br> Country Code: ${giniData[i].getString(
-          "Country Code"
-        )} <br> Gini: ${giniData[i].getNum(
-          "GINI Index"
-        )} <br> Crime: ${crimeData[i].getNum("Homiciderate")}`;
+        tooltipText.innerHTML = `
+          <div class="tooltip-divider"></div>
+          <div class="tooltip-row"><span class="tooltip-label">Country:</span> <span class="tooltip-data">${giniData[
+            i
+          ].getString("Entity")}</span></div>
+          <div class="tooltip-row"><span class="tooltip-label">Country Code:</span> <span class="tooltip-data">${giniData[
+            i
+          ].getString("Country Code")}</span></div>
+          <div class="tooltip-row"><span class="tooltip-label">Gini:</span> <span class="tooltip-data">${giniData[
+            i
+          ].getNum("GINI Index")}</span></div>
+          <div class="tooltip-row"><span class="tooltip-label">Crime:</span> <span class="tooltip-data">${crimeData[
+            i
+          ].getNum("Homiciderate")}</span></div>
+        `;
         tooltip.style.left = mouseX + "px";
         tooltip.style.top = mouseY + "px";
         tooltip.style.display = "block";
@@ -158,6 +262,12 @@ window.addEventListener("DOMContentLoaded", function () {
     const span = document.createElement("span");
     span.id = "year_" + year;
     span.innerText = year;
+    span.addEventListener("click", function () {
+      // Event-Listener hinzufügen
+      yearSlider.value = year;
+      updateYearData(); // Ihre Funktion, die den Slider-Wert verarbeitet
+      updateVisibleYear(year); // Jahr hervorheben
+    });
     yearLabels.appendChild(span);
   }
 
